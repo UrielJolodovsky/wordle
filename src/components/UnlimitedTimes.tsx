61989412ec1d7cad211a2382a037ec239c0cdacd
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-10-23 10:30:54 -0300

    fix style UnlimitedTimes button

--- src/components/UnlimitedTimes.tsx
+++ src/components/UnlimitedTimes.tsx
@@ -2,8 +2,9 @@
 
 export default function UnlimitedTimes() {
     return (
-        <div className="fixed bottom-20 right-28">
-            <div className=" border-wordle p-5 rounded-[25px] border-4 hover:scale-105">
+        <div className="flex flex-direction:row">
+            <div className="w-3/5"></div>
+            <div className="border-wordle p-5 rounded-[25px] border-4 hover:scale-105 justify-end">
             <button className="text-white text-5xl font-black">
                 Get unlimited times
             </button>
