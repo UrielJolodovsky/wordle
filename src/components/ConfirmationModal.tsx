415170111d8ca5394d3446b4c8627ee099777cb1
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2023-11-03 09:36:14 -0300

    change style modal

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -25,27 +25,39 @@ export default function ConfirmationModal () {
                     <Image src="/Close-icon.png" alt="close" width={30} height={30} />
                 </button>
                 </div>
-                <div className="h-1/3 flex flex-col">
+                <div className="h-1/2 flex flex-col">
                 <div className="text-2xl flex justify-start items-start pl-5">
                     <h3 className="text-black">Select language:</h3>
                 </div>
-                <div className="h-1/2 flex flex-direction:row justify-center items-center">
-                    <div className="pt-5 flex gap-5">
-                        <div className="bg-[#9b9b9b] rounded-[15px] p-2">
+                <div className="h-1/2 grid grid-cols-2 justify-center items-center gap-10">
+                    {/* <div className="pt-5 flex gap-5"> */}
+                        {/* <div className="bg-[#9b9b9b] rounded-[15px] p-2">
                         <input type="radio" name="language" id="english" className="mr-2 gap-3"/>
                         <label htmlFor="english">English</label>
                         </div>
                         <div className="bg-[#9b9b9b] rounded-[15px] p-2">
                         <input type="radio" name="language" id="spanish" className="mr-2 gap-3"/>
                         <label htmlFor="spanish">Spanish</label>
+                        </div> */}
+                        <div className="flex justify-center items-center">
+                            <select className="px-5 py-3 border-2 rounded-[10px] bg-greenBtn2">
+                                <option value={""} selected>
+                                    Select a language
+                                </option>
+                                <option value={"Spanish"}>
+                                    Spanish
+                                </option>
+                                <option value={"English"}>
+                                    English
+                                </option>
+                            </select>
+                        </div>
+                    <div className="flex justify-center items-center">
+                        <div className="flex bg-[#9b9b9b] px-5 rounded-[15px]">
+                            <Image src="/spain.png" alt="arrow" width={100} height={100} />
                         </div>
                     </div>
                 </div>
-                </div>
-                <div className="flex justify-center items-center">
-                    <div className="flex bg-[#9b9b9b] px-5 rounded-[15px]">
-                    <Image src="/spain.png" alt="arrow" width={100} height={100} />
-                    </div>
                 </div>
                 <div className="py-5 flex items-center justify-center">
                    <button className="text-black border-2 bg-greenBtn2 px-10 py-3 rounded-[15px] hover:bg-greenBtn" onClick={GoToPlay}>
