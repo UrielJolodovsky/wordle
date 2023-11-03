043f15704b4b7c797a1667943dc80338a0320897
Uriel Jolodosvky <46960341@est.ort.edu.ar>
2023-11-03 15:31:06 -0300

    Continue style of modal

--- src/components/ConfirmationModal.tsx
+++ src/components/ConfirmationModal.tsx
@@ -2,7 +2,7 @@
 import { PageContext } from "@/context/PageContext"
 import { ConfirmationModalProps } from "@/types"
 import { useRouter, useSearchParams } from "next/navigation"
-import { useContext, useEffect, useRef } from "react"
+import { useContext, useEffect, useRef, useState } from "react"
 import Image from 'next/image'
 
 export default function ConfirmationModal () {
@@ -12,6 +12,8 @@ export default function ConfirmationModal () {
     const GoToPlay = () => router.push('/game')
     
     const { isOpen, goFalse } = useContext(PageContext)
+
+    const [language, setLanguage] = useState("")
 
     if (!isOpen) return null;
 
@@ -40,7 +42,7 @@ export default function ConfirmationModal () {
                         <label htmlFor="spanish">Spanish</label>
                         </div> */}
                         <div className="flex justify-center items-center">
-                            <select className="px-5 py-3 border-2 rounded-[10px] bg-greenBtn2">
+                            <select value={language} onChange={(ev: any) => setLanguage(ev.target.value)} className="hover:ease-in-out px-5 py-3 border-2 rounded-[10px] bg-greenBtn2">
                                 <option value={""} selected>
                                     Select a language
                                 </option>
@@ -53,14 +55,18 @@ export default function ConfirmationModal () {
                             </select>
                         </div>
                     <div className="flex justify-center items-center">
-                        <div className="flex bg-[#9b9b9b] px-5 rounded-[15px]">
-                            <Image src="/spain.png" alt="arrow" width={100} height={100} />
+                        <div className="flex bg-[#9b9b9b] px-5 py-3 rounded-[15px]">
+                            {language === "Spanish" ? (
+                                <Image src="/spain.png" alt="arrow" width={100} height={100} />
+                            ) : (
+                                <Image src="/eeuu.png" alt="arrow" width={100} height={100} />
+                            )}
                         </div>
                     </div>
                 </div>
                 </div>
                 <div className="py-5 flex items-center justify-center">
-                   <button className="text-black border-2 bg-greenBtn2 px-10 py-3 rounded-[15px] hover:bg-greenBtn" onClick={GoToPlay}>
+                   <button className="text-black border-2 bg-greenBtn2 px-10 py-3 rounded-[15px] hover:bg-greenBtn hover:ease-in-out" onClick={GoToPlay}>
                     Go to game!
                    </button>
                 </div>
