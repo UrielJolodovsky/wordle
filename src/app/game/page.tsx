6170e21aa678dae732fa785cf7afc87dc62c6ed4
UrielJolodovsky <urieljolo@gmail.com>
2024-01-07 23:15:46 -0300

    center game grid

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -6,9 +6,11 @@ import Header from "@/components/Header";
 
 export default function Home() {
     return (
-        <div className="w-screen h-screen bg-landingColor">
+        <div className="w-screen min-h-screen bg-landingColor">
             <Header inHome={false} />
+            <div className="flex justify-center items-center">
             <GameGrid />
+            </div>
         </div>
     )
 }
